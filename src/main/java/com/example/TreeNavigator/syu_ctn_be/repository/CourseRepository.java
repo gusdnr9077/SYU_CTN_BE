58c6942468edc544ff451c43b7285060b51a3346
gusdnr9077 <95761934+gusdnr9077@users.noreply.github.com>
2026-05-20 14:28:43 +0900

    Delete src/main/java/com/example/TreeNavigator directory

--- src/main/java/com/example/TreeNavigator/syu_ctn_be/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.TreeNavigator.syu_ctn_be.repository;
-import com.example.TreeNavigator.syu_ctn_be.domain.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-public interface CourseRepository extends JpaRepository<Course, Long> {
-}
